3e5c2cb55a878e9d2498d3665c8a7d4b3e73f348
turmary <turmary@126.com>
2019-04-03 19:34:49 +0800

    Update: finace usdhufpro to const cost each time

--- finance/usdhufpro.c
+++ finance/usdhufpro.c
@@ -4,24 +4,27 @@
 int main(int argc, char* argv[]) {
 	double unit_price;
 	double s;
-	double weight = 4.6; //g
+	double weight = 0;  //g
 	double step = 3.0;  //g
 	double money;
+	double m = 1000.0;
 
 	s = 0.0;
 	money = 0.0;
 	for (unit_price = 220.6; unit_price > 143.0; unit_price -= step) {
-		double m = weight * unit_price;
+
 		money += m;
+		weight = m / unit_price;
+
 		s += weight;
 
 		printf("spend=%8.1f loss=-%8.1f price=%4.2f aver=%4.2f weight=%8.2f g\n",
 		              money, (money / s - unit_price) * s, unit_price,  money / s,       weight);
 
-		weight += 4.6 * 0.02;
+		// weight += 4.6 * 0.02;
 	}
 	printf("weight sum    = %8.2f g\n", s);
-	printf("money spend   = %8.1f US $\n", money);
-	printf("average price = %4.2f US $\n", money / s);
+	printf("money spend   = %8.2f US $\n", money);
+	printf("average price = %8.2f US $\n", money / s);
 	return 0;
 }
