c6e2cb0b3878580b0cdff31075ce1361b428e8e7
turmary <turmary@126.com>
2019-05-08 18:29:07 +0800

    Add: strsep experiment code

--- api-examples-unix/strtok.c
+++ api-examples-unix/strtok.c
@@ -1,11 +1,21 @@
+#include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-main() {
+int main(int argc, char* argv[]) {
 	char s[] = "ab-cd: ef;gh: i-jkl;mnop;qrs-tu: vwx-y;z";
 	char* delim = "-: ";
 	char* p;
+	char s2[] = "D  \t5 52 53 54";
+	char* n;
 
-	printf("%s ", strtok(s, delim));
-	while ((p = strtok(NULL, delim))) printf("%s ", p);
+	printf("%s \n", strtok(s, delim));
+	while ((p = strtok(NULL, delim))) printf("%s \n", p);
 	printf("\n");
+
+	p = s2 + 1;
+	while ((n = strsep(&p, " \t")) != NULL) {
+		printf("strsep = %s\n", n);
+	}
+	return 0;
 }
