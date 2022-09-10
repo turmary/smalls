815dd6f7252b03f53847aea3e38690fde3d34fd1
turmary <turmary@126.com>
2022-09-10 22:39:32 +0800

    Update: git bash auto startup script to powershell (more simplified) version

--- bat-file/git-bash-auto.cmd
+++ bat-file/git-bash-auto.cmd
@@ -1,67 +1,21 @@
 ::::::::::::::::::::::::::::::::::::::::::::
-:: Elevate.cmd - Version 4
 :: Automatically check & get admin rights
 :: see "https://stackoverflow.com/a/12264592/1016343" for description
 ::::::::::::::::::::::::::::::::::::::::::::
- @echo off
- CLS
- ECHO.
- ECHO =============================
- ECHO Running Admin shell
- ECHO =============================
- 
-:init
- setlocal DisableDelayedExpansion
- set cmdInvoke=1
- set winSysFolder=System32
- set "batchPath=%~0"
- for %%k in (%0) do set batchName=%%~nk
- set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
- setlocal EnableDelayedExpansion
- 
-:checkPrivileges
-  NET FILE 1>NUL 2>NUL
-  if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
- 
-:getPrivileges
-  if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
-  ECHO.
-  ECHO **************************************
-  ECHO Invoking UAC for Privilege Escalation
-  ECHO **************************************
- 
-  ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
-  ECHO args = "ELEV " >> "%vbsGetPrivileges%"
-  ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
-  ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
-  ECHO Next >> "%vbsGetPrivileges%"
- 
-  if '%cmdInvoke%'=='1' goto InvokeCmd 
- 
-  ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
-  goto ExecElevation
- 
-:InvokeCmd
-  ECHO args = "/c """ + "!batchPath!" + """ " + args >> "%vbsGetPrivileges%"
-  ECHO UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
- 
-:ExecElevation
- "%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
- exit /B
- 
-:gotPrivileges
- setlocal & cd /d %~dp0
- if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
- 
+@setlocal enabledelayedexpansion enableextensions
+@echo off
+
+:: Admin check
+rem :: (echo !_!)&
+fltmc >nul 2>nul || set _=^"set _ELEV=1^& cd /d "%cd%"^& "%~f0" %* ^"&&((if "%_ELEV%"=="" (echo Requesting administrator privileges...&((powershell -nop -c start cmd -args '/d/x/s/r',$env:_ -verb runas -windowstyle hidden >nul 2>nul) || (mshta vbscript:execute^("createobject(""shell.application"").shellexecute(""cmd"",""/d/x/s/v:off/r ""&createobject(""WScript.Shell"").Environment(""PROCESS"")(""_""),,""runas"",1)(window.close)"^) >nul 2>nul))) else (echo This script requires administrator privileges.& pause))& exit /b)
+
+:: echo Has admin permissions& echo Working dir: "%cd%"& echo Script dir: "%~dp0"& echo Script path: "%~f0"& echo Args: %*& pause
+
  ::::::::::::::::::::::::::::
  ::START
  ::::::::::::::::::::::::::::
  REM begin real part with administrator privilege
 
-
-@echo off
-SETLOCAL ENABLEEXTENSIONS
-SETLOCAL ENABLEDELAYEDEXPANSION
 
 cd /d %HOMEDRIVE%%HOMEPATH%
 start d:\TaryInst\Git\git-bash.exe --cd-to-home
