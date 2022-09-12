3e80790e52ec0149cbc09d14bad3963bf7b7f0fc
turmary <turmary@126.com>
2022-09-12 12:43:14 +0800

    Add: bash history control into msys/setup-env.sh

--- msys/setup-env.sh
+++ msys/setup-env.sh
@@ -225,6 +225,24 @@ alias egrep &> /dev/null || {
 	echo "alias egrep='egrep --color'" >> $CONF
 }
 
+grep -E "HISTCONTROL" $CONF &> /dev/null || cat >> $CONF <<-\__EOF__
+	# don't put duplicate lines or lines starting with space in the history.
+	# See bash(1) for more options
+	HISTCONTROL=ignoreboth:erasedups
+	HISTIGNORE='history:ls *:ps *:pwd'
+
+	# append to the history file, don't overwrite it
+	shopt -s histappend
+
+	# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+	HISTSIZE=10000
+	HISTFILESIZE=20000
+
+	# check the window size after each command and, if necessary,
+	# update the values of LINES and COLUMNS.
+	shopt -s checkwinsize
+__EOF__
+
 [ "$SSH_AGENT_PID" ] || install_ssh_agent
 
 source $CONF
