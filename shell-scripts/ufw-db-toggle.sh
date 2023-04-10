7bba5bcbe0ce70d45eb204e208be8abb405a1bd8
linsheng.yang <linsheng.yang@anker.com>
2023-04-10 16:29:56 +0800

    Add: killcx for ufw-db-toggle.sh

--- shell-scripts/ufw-db-toggle.sh
+++ shell-scripts/ufw-db-toggle.sh
@@ -10,12 +10,14 @@ SRVIPDOT=$(echo $SRVIP | sed -ne 's,\.,\\\.,gp')
 echo SRVIP=$SRVIP
 
 while true; do
+    # https://killcx.sourceforge.net/
+    killcx $SRVIP:3306
     ufw deny out from any to $SRVIP || exit 2
 
     # nr=$(ufw status numbered | sed -nre "s,^\[ *([0-9]+)\] +$SRVIPDOT.*,\1,gp")
       nr=$(ufw status numbered | sed -ne  "s,^\[ *\([0-9]\+\)] \+$SRVIPDOT.*,\1,gp")
-    sleep 2
+    sleep 20
     [ "$nr" ] && ufw --force delete $nr
-    sleep 2
+    sleep 20
 done
 
